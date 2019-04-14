196a2eb6ffeba4bb351afd3784c49ded0e48ab78
david7482 <david74.chou@gmail.com>
2019-04-14 22:32:02 +0800

    Update ec2 IAM ids

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -6,7 +6,7 @@ variable "ami" {
   type = "map"
 
   default = {
-    "ap-northeast-1" = "ami-0c5c5f2328012a2d4"
-    "us-east-1"      = "ami-0a50b5b2d99760cf2"
+    "ap-northeast-1" = "ami-0ad15a5a72c4f8bb6"
+    "us-east-1"      = "ami-0fa3a798d9d07f987"
   }
 }
